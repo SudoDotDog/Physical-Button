2fb4e624c5093f13eab9d2690657f23301516319
WMXPY <wm@mengw.io>
2021-03-09 14:03:34 -0600

    rename dog cinabyt namne

--- android/src/main/java/dog/sudo/physical_button/PhysicalButtonPlugin.java
+++ android/src/main/java/dog/sudo/physical_button/PhysicalButtonPlugin.java
@@ -9,12 +9,10 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** PhysicalButtonPlugin */
 public class PhysicalButtonPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+  private static final String ACCELEROMETER_CHANNEL_NAME =
+      "plugins.flutter.io/sensors/accelerometer";
+  
   private MethodChannel channel;
 
   @Override
